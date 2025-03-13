ddf002d6df9368f1fca143a12e446bf5d7d684c6
nishit009 <150333141+nishit009@users.noreply.github.com>
2025-03-13 17:45:43 +0530

    hello start

---
+++ spotify/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="w-full h-screen text-green-500 flex justify-center items-center font-bold text-9xl ">
+        hell0
+      </div>
+    </>
+  );
+}
+
+export default App;
